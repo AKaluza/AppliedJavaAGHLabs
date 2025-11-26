d631eb47ae9eb6bf3534d20f9acc9946136063ac
andrzej <andrzejkaluza92@gmail.com>
2025-11-26 19:12:59 +0100

    Refaktor Average

--- Introduction/Arrays/Average/src/Average.java
+++ Introduction/Arrays/Average/src/Average.java
@@ -1,11 +1,15 @@
 // Average an array of values.
    public class Average {
-   public static void main(String[] args) {
-      double[] nums = {10.1, 11.2, 12.3, 13.4, 14.5};
+   public static double average(double[] arr) {
       double result = 0;
       int i;
-      for(i=0; i<5; i++)
-         result = result + nums[i];
-      System.out.println("Average is " + result / 5);
+      for(i=0; i<arr.length; i++)
+         result = result + arr[i];
+      return result / arr.length;
+   }
+
+   public static void main(String[] args) {
+      double[] nums = {10.1, 11.2, 12.3, 13.4, 14.5};
+      System.out.println("Average is " + Average.average(nums) );
    }
 }
--- Introduction/Arrays/Average/task.md
+++ Introduction/Arrays/Average/task.md
@@ -1,6 +1,15 @@
-calculate average of numbers:
-10.1, 11.2, 12.3, 13.4, 14.5
+You are given an array of five floating-point numbers:
 
-### output:
-```
-Average is 12.299999999999999
+double[] nums = {10.1, 11.2, 12.3, 13.4, 14.5};
+
+
+Your job is to:
+
+Loop through all elements of the array using a for loop.
+
+- Compute the total sum of all numbers. 
+- Compute the average by dividing the sum by the number of elements. 
+- Return the result in the format:
+- In Main print: Average is X
+
+Where X is the computed average.
